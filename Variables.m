8417f3d76c57d3958fd2f44e610b27ab6e43c54d
lautarodc <ldc0295@gmail.com>
2019-12-17 00:56:20 -0300

    Actualización y prueba de salida de la planta. Modelo de contacto

--- Variables.m
+++ Variables.m
@@ -2,23 +2,20 @@
 rc=15;      %Reducción
 Rw=0.5;     %m
 Jwc=2;      %kg.m^2
-Mt=50000;   %kg
+Mc=50000;   %kg
 Jmc=10;     %kg.m^2
 beq1=30;    %Nm/(rad/s)---bwc+bmc*(rc^2)
-bmc=0;      %A definir
-bwc=0;      %A definir
+bmc=15;      %A definir
+bwc=15;      %A definir
 Jeq1=Jwc+Jmc*(rc^2);
-bt=0;
 %% Variables Izaje
-Mh=0;       %A definir
 riz=30;
 Rd=0.75;    %m
 Jd=8;       %kg.m^2
 Jmi=30;     %kg.m^2
 beq2=18;    %Nm/(rad/s)---bd+bmi*(ri^2)
-bmi=0;      %A definir
-bd=0;       %A definir
-bh=0;
+bmi=9;      %A definir
+bd=9;       %A definir
 Jeq2=Jd+Jmi*(riz^2);
 %% Variables péndulo
 Kw=1800e3;  %N/m
@@ -29,4 +26,7 @@ ml_min=17000;
 %% Variables estructurales
 Y_t0=45;
 g=9.81;     %m/s^2
+kcy=1.3e11; %N/m
+bcy=500e3;  %N/m/s
+bcx=1000e3; %N/m/s
 
