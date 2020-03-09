2eb045df3ede6c4d89e13d198991060d2a031f1c
lautarodc <ldc0295@gmail.com>
2020-03-09 12:36:51 -0300

    Se modifica el HDMI para agregar graficas en tiempo de ejecución y consigna de carga/descarga

--- PlanificacionCargas.m
+++ PlanificacionCargas.m
@@ -22,7 +22,7 @@ capacidad_lateral=((barco_x(1,3)-barco_x(1,2))/l_cont);
 capacidad_vertical=((25-barco_y(1,2))/l_cont);
 %% Escenario aleatorio de contenedores en muelle y barco
 capacidad_Total=capacidad_lateral*capacidad_vertical;
-ocupacion=floor(0.5*capacidad_Total);
+ocupacion=floor(0.7*capacidad_Total);
 %Creacion de contenedor en muelle
 % [cont_x,cont_y]=contenedor(lim_xi+(lim_xd-lim_xi)*rand(),1.25,l_cont);
 xo=-10;
@@ -62,7 +62,7 @@ axis([-30 60 -30 50],'equal')
 [temp, order] = sort(location(1,:));
 contenedores_ordenados = location(:,order);
 barco_contenedores=zeros(18,18);
-length(contenedores_ordenados)
+length(contenedores_ordenados);
 for i=1:length(contenedores_ordenados)
     barco_contenedores(contenedores_ordenados(1,i),contenedores_ordenados(2,i))=contenedores_ordenados(3,i);
 end
@@ -141,29 +141,67 @@ columna_consigna=4; %Columna de consigna a elegir
 xc=5+2.5*columna_consigna;
 index_columna=((30+xc)/0.5)+1;
 index_contenedor=((30+xo)/0.5)+1;
-h_max=max(ynuevo(index_contenedor:index_columna));
+h_max=max(ynuevo(index_contenedor:(index_columna-1)));
 i=1;
 while(ynuevo(i)~=h_max)
     i=i+1;
 end
-x_max=x(i);
+x_max=x(i)+1.25;
 vel_max_izaje=2;
 vel_max_carro=4;
 x_velmax=8;
-distx_total=x_max-xo+1.25;
+distx_total=x_max-xo-1.25;
 dif_x=distx_total-x_velmax;
 temp_total=4+dif_x/vel_max_carro;
 aux_y=temp_total*vel_max_izaje;
 subir_y=h_max-aux_y;
 temp_y=sqrt(2*subir_y/1);
 temp_total=temp_total+temp_y;
+t=linspace(0,temp_total,100);
+for i=1:length(t)
+    if t(i)<temp_y
+        x_t(i)=xo+1.25;
+        y_t(i)=0.5*1*(t(i)*t(i));
+        
+    else
+        if t(i)<(temp_y+4)
+            x_t(i)=xo+1.25+0.5*1*(t(i)-temp_y)*(t(i)-temp_y);
+        else
+            x_t(i)=xo+1.25+x_velmax+vel_max_carro*(t(i)-temp_y-4);
+        end
+        y_t(i)=subir_y+vel_max_izaje*(t(i)-temp_y);
+    end
+end
+
+
 plot([(xo+1.25) (xo+1.25)],[0 subir_y],'g');
 x_recta=linspace(xo+1.25,x_max,10);
 for i=1:length(x_recta)
-   y_recta(i)=subir_y+((h_max-subir_y)/(x_max-(xo+1.25)))*(x_recta(i)-(xo+1.25)); 
+    y_recta(i)=subir_y+((h_max-subir_y)/(x_max-(xo+1.25)))*(x_recta(i)-(xo+1.25));
 end
 plot(x_recta,y_recta,'g');
+plot(x_t,y_t,'r')
 
+
+%% Prueba de armado de grafica en UI en funcion de la envolvente de contacto
+
+figure(2)
+hold on
+axis([-30 60 -30 50],'equal');
+ind_muelle=((30+0)/0.5)+1;
+ind_barco=((30+5)/0.5)+1;
+% stairs(x(ind_barco:length(x)),y(ind_barco:length(x)),'m');
+for i=ind_barco:5:length(x)-1
+    cant_col=(y(i)-(-20))/2.5;
+    if cant_col~=0
+        for j=1:cant_col
+            [cont_x,cont_y]=contenedor(x(i)+1.25,-20+2.5*(j-1)+1.25,2.5);
+            plot(cont_x,cont_y,'m-')
+        end
+    end
+end
+plot(base_x,base_y,'k-',barco_x,barco_y,'g-',mar_x1,mar_y1,'b-',mar_x2,mar_y2,'b-',carro_x,carro_y,'k-');
+stairs(x(1:ind_muelle),y(1:ind_muelle),'k-')
 
 
 
